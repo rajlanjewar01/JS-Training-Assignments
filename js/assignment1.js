1d5f497fd4611a331f2ef5718b59b2a2cf3bf520
rajperficient01 <rajl.perficient@gmail.com>
2022-01-09 16:01:09 +0530

    added js logic for assgn 1

--- js/assignment1.js
+++ js/assignment1.js
@@ -5,4 +5,15 @@ form.addEventListener("submit", function(e){
     var inputAmt = document.querySelector("#amt").value;
     inputAmt = parseInt(inputAmt);
     
+    //check conditions
+    if(inputAmt >= 35){
+        var leftAmt = inputAmt - 35;
+        alert("Ashish was able to purchase Grossary and Ice Cream and Amount left with him is: (Rs) " + leftAmt);
+    }
+    else if(inputAmt === 20 || inputAmt < 35){
+        alert("Ashish was only able to purchase Grossary with given amount");
+    }
+    else{
+        alert("Ashish wasn't able to purchase due to insufficient amount");
+    }
 });
